162011a1cd5513b1dccf713fe8f36ce2fae4edbc
SashaKur <101345117+SashaKur@users.noreply.github.com>
2023-04-29 14:00:49 +0200

    Project initialization

---
+++ src/main/java/com/sashakur/featurematcher/FeatureMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.sashakur.featurematcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sashakur/featurematcher/FeatureMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashakur.featurematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
